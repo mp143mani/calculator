128fdf7fe9ed23190f8d16e47fd6338082202f9f
MANIVENKATRAJ M <127532509+mp143mani@users.noreply.github.com>
2023-08-26 10:54:58 +0530

    s

--- script.js
+++ script.js
@@ -1,172 +1,222 @@
-var result = document.getElementById('result');
-var smallResult = document.getElementById('smallResult');
-var cal = false;
-var val_1 = false;
-var val_2 = false;
-var oper = '+';
-var operPressed = false;
-var tot = 0;
-var cal_done = false;
-var currentStatus = 0;
-var o;
+//creating calculater
 
-function num(val) {
-	val = val.toString(); // converting to string so it won't be added together in display (2 + '2' should not do '22')
 
-	if (cal_done)
-		// if calculation is done
-		cls(); // clear everything
+let D =  document.createElement('div');   //creating container
+document.body.appendChild(D);
+D.setAttribute("class","container  col-6 col-md-4 col-xl-3  mt-5  ") 
 
-	if (!operPressed) {
-		// if no operator is pressed (means it's first value)
-		if (!val_1) val_1 = 0;
+let Calculator =  document.createElement('div'); //creating the div for the alignment 
+D.appendChild(Calculator);
+Calculator.setAttribute("class","row cal-c col-10 col-lg-8 d-flex gap-2 shadow-lg flex-column align-content-center rounded-3 pb-2 ")
 
-		val_1 = val_1 + val;
 
-		val_1 = lengthFix(val_1); // limiting to 12
 
-		result.innerHTML = val_1;
-		smallResult.innerHTML = val_1;
-		// console.log('num to be calc: ' + val_1);
-	}
-	if (operPressed) {
-		// if operator is pressed (means it's 2nd value)
-		if (!val_2) val_2 = 0;
 
-		val_2 = val_2 + val;
 
-		val_2 = lengthFix(val_2); // limiting to 12
+let display =  document.createElement('div');                       // creating the div to display the result 
+display.setAttribute("class"," row display  rounded-top  pt-4 mb-2")
 
-		result.innerHTML = val_2;
-		smallResult.innerHTML = val_1 + oper + val_2;
-		// console.log('2nd num to be calc: ' + val_2);
-	}
+let result =  document.createElement('input');                     // creating the input element as display 
+result.setAttribute("type"," text")
+result.setAttribute("class"," fs-3 fw-bolder border-0   text-end h-50  mt-2 rounded-3 ")
+result.setAttribute("id","display")
+result.setAttribute("placeholder"," 0")
+result.setAttribute("name", "displayResult")
+
+display.appendChild(result);                                       // inserting the display 
+Calculator.append(display);
+
+
+
+
+ let Texts = document.createElement('div');                                         // creating row the buttons for clear and memmory
+ Texts.setAttribute("class"," row number  gap-1  d-inline-flex  flex-nowrap")
+ Calculator.appendChild(Texts);
+
+
+
+ let buttonC = document.createElement('button');                                    // creating  the buttons for clear and memmory
+ buttonC.innerText = 'C'
+ buttonC.setAttribute("class","  col btn btn-success p-md-3 rounded-circle w-25")
+
+
+ 
+ let buttonMc = document.createElement('button');
+ buttonMc.innerText = 'Mc'
+ buttonMc.setAttribute("class","  col btn  btn-primary py-md-2 align-self-md-center rounded-circle w-25 ")
+
+ 
+ let buttonMadd = document.createElement('button');
+ buttonMadd.innerText = 'M+'
+ buttonMadd.setAttribute("class","  col btn  btn-primary py-md-2 align-self-md-center rounded-circle w-25 ")
+
+ 
+ let buttonMremove = document.createElement('button');
+ buttonMremove.innerText = 'M-'
+ buttonMremove.setAttribute("class","  col btn  btn-primary py-md-2 align-self-md-center rounded-circle w-25 ")
+
+
+ Texts.append(buttonC,buttonMc,buttonMadd,buttonMremove);                         // inserting all  the buttons 
+ buttonC.innerText = 'C'
+
+
+
+
+
+
+
+var Numerical=[['7','8','9','+'],['4','5','6','-']
+,['1','2','3','*'],['.','0','/','=']]
+
+
+for(var i=0; i<=3;i++){                                                         
+var B =  Numerical[i]
+  
+let number = document.createElement('div');                            //creating the row buttons for the number and arithmatic sign
+number.setAttribute("class"," row gap-2  d-inline-flex flex-nowrap")
+    for(var j=0; j<=3;j++){
+        
+    let button = document.createElement('button');                   //creating the row buttons for the number and arithmatic sign
+    button.innerText = B[j]
+    button.setAttribute("class"," col btn  btn-primary  rounded-circle w-25")
+         number.append(button);                                       // inserting all  the buttons 
+         } 
+
+Calculator.appendChild(number);
 }
 
-// when /,*,+,- is clicked
-function calc(val) {
-	if (val_1 && val_2) {
-		operPressed = true;
-		total();
-		oper = val;
-	}
 
-	if (cal_done) {
-		var x = (val_1 = tot);
-		cls();
-		val_1 = x;
-		val_1 = lengthFix(val_1); // limiting to 12
-		result.innerHTML = val; // display which operator is selected
-		// var a = smallResult.innerHTML.toString();
-		smallResult.innerHTML = +x + val;
-		oper = val;
-		operPressed = true;
-	}
 
-	if (!val_1 || operPressed) {
-		return false;
-	}
 
-	if (val_1 && !val_2) {
-		result.innerHTML = val; // display which operator is selected
-		var a = smallResult.innerHTML.toString();
-		smallResult.innerHTML = a + val;
-		oper = val;
-		operPressed = true;
-	}
-}
 
-function total() {
-	if (!val_1) return false;
 
-	if (!val_2 && operPressed) {
-		tot = magic(val_1, val_1, oper);
-		tot = lengthFix(tot);
-	}
 
-	if (val_1 && val_2) {
-		tot = magic(val_1, val_2, oper);
-		tot = lengthFix(tot);
-	}
+// //creating functions to calculate//
 
-	tot = tot.toString();
-	var noDec = tot.indexOf('.') == -1;
-	if (!noDec) tot = parseFloat(tot).toFixed(3);
 
-	result.innerHTML = tot;
-	// smallResult.innerHTML = tot;
-	// console.log('total: ' + tot);
-}
+function calculation(){
 
-function magic(a, b, oper) {
-	switch (oper) {
-		case '+':
-			tot = +a + +b;
-			cal_done = true;
-			break;
-		case '-':
-			tot = +a - +b;
-			cal_done = true;
-			break;
-		case '/':
-			tot = +a / +b;
-			cal_done = true;
-			break;
-		case '*':
-			tot = +a * +b;
-			cal_done = true;
-			break;
-		default:
-			return false;
-	}
-	return tot;
-}
 
-// clearing everything...
-function cls() {
-	smallResult.innerHTML = '';
-	result.innerHTML = 0;
-	val_1 = false;
-	val_2 = false;
-	oper = '+';
-	tot = 0;
-	cal_done = false;
-	operPressed = false;
-}
 
-function lengthFix(o) {
-	o = o.toString();
 
-	if (o != 0 || o != '0') {
-		if (o.charAt(0) == 0) o = o.slice(1);
-	}
 
-	if (o.toString().length > 12) o = o.substring(0, 12);
 
-	return o;
-}
+function listner() { 
+    var button  =  document.getElementsByTagName("button");                         // Find all <button> tags in the document
 
-document.onkeyup = function(e) {
-	if (e.which == 110 || e.which == 190) num('.');
-	if (e.which == 96 || e.which == 48) num('0');
-	if (e.which == 97 || e.which == 49) num('1');
-	if (e.which == 98 || e.which == 50) num('2');
-	if (e.which == 99 || e.which == 51) num('3');
-	if (e.which == 100 || e.which == 52) num('4');
-	if (e.which == 101 || e.which == 53) num('5');
-	if (e.which == 102 || e.which == 54) num('6');
-	if (e.which == 103 || e.which == 55) num('7');
-	if (e.which == 104 || e.which == 56) num('8');
-	if (e.which == 105 || e.which == 57) num('9');
 
-	if (e.which == 111) calc('/');
-	if (e.which == 106) calc('*');
-	if (e.which == 107) calc('+');
-	if (e.which == 109) calc('-');
+   
+    for(var i = 0; i <= button.length-1; i++) {
+    button[i].addEventListener("click", entered);                               // Loop through them, adding an event handler for "click" events to each
+  
+    }
 
-	if (e.which == 13) total();
-
-	if (e.which == 8 || e.which == 46) cls();
-
-	if (e.which == 27) cls();
+ 
 };
+
+listner();                                                                  // invocation of addevent function
+
+
+
+
+
+var display  =  document.getElementById("display");                         //getting the input element 
+let text =       document.createElement("span");
+text.setAttribute("class","overflow-hidden")
+
+var memmory=[];                                                                // creating array for memmory(M)
+
+
+
+
+     function entered (event){                                                  // forming the listener function for arithmatic operation
+
+
+            if( (event.target.innerText.match(/[^a-zA-Z=]/))                   //filltering digits for entry
+             &&  (event.target.innerText !=='M+')   
+             && (event.target.innerText !=='M-') )
+             {
+
+        display.value += event.target.innerText
+        
+    }
+
+
+    if( event.target.innerText==="C"){                                         // to clear display
+        display.value = ""
+    }
+
+
+    if( event.target.innerText==="="){                                         // to evaluate the arithmatic operation
+        
+      display.value = eval(  display.value);
+       
+    }
+
+
+
+
+    if( event.target.innerText==="M+"){                                       // to memmorise the number
+        memmory.push(display.value);
+        display.value = ""
+       
+        text.innerText = memmory;
+ display.parentNode.appendChild(text);
+    }
+
+    if( event.target.innerText==="M-"){                                       // to  delete memmorised number
+        memmory.shift(display.value);
+        text.innerText = memmory;
+    }
+    if( event.target.innerText==="Mc"){                                       // to  clear all memmorised number
+        memmory.splice(0, memmory.length);
+        text.innerText = memmory;  
+    }
+
+
+}
+
+
+window.addEventListener("keydown", enteredKey);                             //Adding the keyboard key events
+
+
+
+function enteredKey(e){                                                         //keyboard listener function
+
+
+
+    if( (e.key).match(/[0-9]/)||e.key ==="+"                                  //to get numbers
+    ||e.key ==="-"||e.key ==="*"||e.key ==="/"){
+      
+        display.value += e.key;
+
+      console.log(e.key)
+    }
+
+
+    else if (e.key==="Escape" ){                                                //to clear the number
+        display.value = "";
+         }
+
+
+    else if (e.key==="Enter"){                                                   // to evaluate calculation
+        display.value = eval( display.value);
+        console.log("hello")
+         }
+      
+
+         else if ((e.key).match(/[A-Za-z]/)&&(e.key!=="ArrowRight")              // to alert the user to enter 
+         &&(e.key!=="ArrowLeft")&&(e.key!=="Backspace" ) ) {
+            display.value = "" ;
+            alert("please enter number");
+        
+        
+        }
+ 
+    
+}
+}
+
+
+
+window.onload = calculation();                                   //invocation of calculation function
